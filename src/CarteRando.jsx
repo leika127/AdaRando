8f390a029aeb292f13412ed3ba0d71152ca17847
leika127 <afrx35@gmail.com>
2026-08-28 15:23:11 +0200

    ajout CarteRando.jsx et Entete.jsx

---
+++ src/CarteRando.jsx
@@ -0,0 +1,13 @@
+function CarteRando({ rando }) {
+  return (
+    <div className="carte-rando">
+      <h2>{rando.nom}</h2>
+      <p>Difficulté : {rando.difficulte}</p>
+      <p>Durée : {rando.duree_h}h</p>
+      <p>Dénivelé : {rando.denivele_m}m</p>
+      {rando.balisee && <p className="badge">📍 Balisée</p>}
+    </div>
+  )
+}
+
+export default CarteRando 
---
+++ src/Entete.jsx
@@ -0,0 +1,10 @@
+function Entete() {
+  return (
+    <div>
+      <h1>AdaRando</h1>
+      <p>Découvrez les meilleures randonnées</p>
+    </div>
+  )
+}
+
+export default Entete 
